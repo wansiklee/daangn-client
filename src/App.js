e65f1b644e6f1a7044049d3dad82c5722ef8923e
wansiklee <dhkstlr2662@gmail.com>
2020-02-12 13:07:51 +0900

    Setting Pages with React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./themes/GlobalStyles";
 import Theme from "./themes/Theme";
 
+import Home from "./pages/Home";
+
 const App = () => {
   return (
     <ThemeProvider theme={Theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </Router>
       <GlobalStyles />
     </ThemeProvider>
   );
