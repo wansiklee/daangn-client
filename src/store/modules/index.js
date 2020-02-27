afe1f5b15dcd7547bbc59381790786bd4588929b
wansiklee <dhkstlr2662@gmail.com>
2020-02-27 19:11:45 +0900

    Redux Loading Module

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import auth from "./auth";
+import loading from "./loading";
 
-export default combineReducers({
-  auth
+const rootReducer = combineReducers({
+  auth,
+  loading
 });
+
+export default rootReducer;
