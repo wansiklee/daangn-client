407f5550021ce8bf9c3963135c11c97cd47ccc20
wansiklee <dhkstlr2662@gmail.com>
2020-03-23 11:16:21 +0900

    Update categoryOptions

--- src/containers/product/ListContainer.js
+++ src/containers/product/ListContainer.js
@@ -7,7 +7,6 @@ const ListContainer = () => {
   const [category, setCategory] = useState();
 
   const categoryOptions = [
-    { value: 0, label: "디지털/가전" },
     { value: 1, label: "가구/인테리어" },
     { value: 2, label: "유아동/유아도서" },
     { value: 3, label: "생활/가공식품" },
@@ -19,11 +18,12 @@ const ListContainer = () => {
     { value: 9, label: "게임/취미" },
     { value: 10, label: "도서/티켓/음반" },
     { value: 11, label: "반려동물용품" },
-    { value: 12, label: "기타 중고물품" }
+    { value: 12, label: "디지털/가전" },
+    { value: 13, label: "기타 중고물품" }
   ];
 
   const onCategoryChange = ({ target: { value } }) => {
-    setCategory(value ? Number(value) : undefined);
+    setCategory(value ? Number(value) : false);
   };
 
   useEffect(() => {
--- src/containers/product/UploadContainer.js
+++ src/containers/product/UploadContainer.js
@@ -13,7 +13,6 @@ const UploadContainer = ({ history }) => {
   const [error, setError] = useState(null);
 
   const categoryOptions = [
-    { value: 0, label: "디지털/가전" },
     { value: 1, label: "가구/인테리어" },
     { value: 2, label: "유아동/유아도서" },
     { value: 3, label: "생활/가공식품" },
@@ -25,7 +24,8 @@ const UploadContainer = ({ history }) => {
     { value: 9, label: "게임/취미" },
     { value: 10, label: "도서/티켓/음반" },
     { value: 11, label: "반려동물용품" },
-    { value: 12, label: "기타 중고물품" }
+    { value: 12, label: "디지털/가전" },
+    { value: 13, label: "기타 중고물품" }
   ];
 
   const onDrop = async image => {
