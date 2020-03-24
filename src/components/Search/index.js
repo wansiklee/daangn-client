8dc44464e6ea5cd01d7b9d51741df37eee37548d
wansiklee <dhkstlr2662@gmail.com>
2020-03-24 15:22:55 +0900

    Styling search results UI

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,7 +1,41 @@
 import React from "react";
+import styled from "styled-components";
+import ResultCard from "../common/ResultCard";
 
-const Search = () => {
-  return <div>Search</div>;
+const SearchResult = styled.div`
+  background: #f8f9fa;
+  padding: 100px 0 40px 0;
+  .wrapper {
+    border-radius: 8px;
+    border: 1px solid #e9ecef;
+    width: 800px;
+    margin: 0 auto;
+    margin-bottom: 20px;
+    background: #fff;
+    .products-wrapper {
+      padding: 0 40px;
+      .wrapper-title {
+        font-weight: 600;
+        color: #212529;
+        font-size: 18px;
+        margin: 20px 0;
+      }
+    }
+  }
+`;
+
+const Search = ({ products, onClick, page, lastPage }) => {
+  return (
+    <SearchResult>
+      <div className="wrapper">
+        <div className="products-wrapper">
+          <p className="wrapper-title">중고거래</p>
+          {products.length !== 0 &&
+            products.map((p, i) => <ResultCard key={i} product={p} />)}
+        </div>
+      </div>
+    </SearchResult>
+  );
 };
 
 export default Search;
---
+++ src/components/common/ResultCard.js
@@ -0,0 +1,71 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { getPriceComma } from "../../lib/utils";
+
+const ProductsList = styled.div`
+  position: relative;
+  text-align: left;
+  display: inline-block;
+  width: 217px;
+  margin-right: 33px;
+  :nth-child(4n) {
+    margin-right: 0;
+  }
+  margin-bottom: 40px;
+`;
+
+const SLink = styled(Link)`
+  display: block;
+  text-decoration: none;
+  color: #212529;
+`;
+
+const Image = styled.div`
+  height: 160px;
+  overflow: hidden;
+  background-color: #f8f9fa;
+  border-radius: 8px;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const Info = styled.div`
+  .title {
+    display: block;
+    font-weight: 600;
+    color: #212529;
+    font-size: 16px;
+    line-height: 18px;
+    margin-top: 10px;
+  }
+  .card-price {
+    font-weight: 600;
+    color: #ff8a3d;
+    font-size: 15px;
+    line-height: 18px;
+    margin-top: 6px;
+  }
+`;
+
+const ResultCard = ({ product }) => {
+  return (
+    <ProductsList>
+      <SLink to={`/products/${product.id}`}>
+        <Image>
+          <img src={`/products/${product.image}`} />
+        </Image>
+        <Info>
+          <div className="card-title">
+            <span className="title">{product.title}</span>
+          </div>
+          <p className="card-price">{getPriceComma(product.price)}원</p>
+        </Info>
+      </SLink>
+    </ProductsList>
+  );
+};
+
+export default ResultCard;
